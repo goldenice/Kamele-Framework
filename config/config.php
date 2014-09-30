1906aa595c3c50c62ca80a981db311c6752ad7d4
Rick Lubbers <ik@ricklubbers.nl>
2014-09-30 08:53:56 +0200

    Cleaned up baseurl config

--- config/config.php
+++ config/config.php
@@ -7,15 +7,16 @@ $config['BASEURL'] = '';
 // Set the BASEURL automatically if none was provided
 if ($config['BASEURL'] == '') {
 	if (isset($_SERVER['HTTP_HOST'])) {
-		$base_url = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
-		$base_url .= '://'. $_SERVER['HTTP_HOST'];
-		$base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-	} else {
-		$base_url = 'http://localhost/';
+		$config['BASEURL'] = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
+		$config['BASEURL'] .= '://'. $_SERVER['HTTP_HOST'];
+		$config['BASEURL'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+	} 
+	else {
+		$config['BASEURL'] = 'http://localhost/';
 	}
 
-	$config['BASEURL'] = $base_url;
-	define('BASEURL', $base_url);
-} else {
+	define('BASEURL', $config['BASEURL']);
+} 
+else {
 	define('BASEURL', $config['BASEURL']);
 }
