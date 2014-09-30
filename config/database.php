bf525b31faad868c399ee0722b53a618b8f88d0f
Rick Lubbers <ik@ricklubbers.nl>
2014-09-30 08:55:19 +0200

    Added type config for databases

--- config/database.php
+++ config/database.php
@@ -3,6 +3,7 @@
  * Defines connection info for the database class
  */
 define('DB_HOST', 'localhost');
+define('DB_TYPE', 'mysql');
 define('DB_USER', '');
 define('DB_PASS', '');
 define('DB_NAME', '');
